a0e3db387221c4379ac9927b8419bc042af67bdb
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2025-04-15 10:16:01 -0700

    Attempts at making sense of post-processing and enemy attacking

--- Parabasis/Assets/Scripts/Enemy Scripts/EnemyAttacking.cs
+++ Parabasis/Assets/Scripts/Enemy Scripts/EnemyAttacking.cs
@@ -1,11 +1,23 @@
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAttacking : MonoBehaviour
 {
     public RaycastHit2D hit;
     public Transform player;
+    public bool attacking;
     [SerializeField] private float attackRange;
+    [SerializeField] private float timer;
+    [SerializeField] private float attackLimit;
+    [SerializeField] private float damage;
+    [SerializeField] private bool Pierce;
+    [SerializeField] private bool Slash;
+    [SerializeField] private bool Blunt;
+    [SerializeField] private bool AN;
+    [SerializeField] private bool Burn;
+    [SerializeField] private int burnAmount;
+    [SerializeField] private int burnDamage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,7 +35,31 @@ public class EnemyAttacking : MonoBehaviour
 
         if (Physics2D.Raycast(transform.position, directionToPlayer, attackRange))
         {
-            
+            attacking = true;
+        }
+        else
+        {
+            attacking = false;
+        }
+
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= attackLimit)
+            {
+                attacking = false;
+                timer = 0;
+            }
+        }
+
+        if (attacking)
+        {
+            if (hit.collider.GetComponent<Health>() != null)
+            {
+                Health health = hit.collider.GetComponent<Health>();
+                health.Damage(damage, Pierce, Slash, Blunt, AN, Burn, burnAmount, burnDamage);
+            }
         }
     }
 }
