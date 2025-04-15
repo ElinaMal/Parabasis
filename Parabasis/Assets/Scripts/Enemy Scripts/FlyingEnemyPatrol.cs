80a3d4962c517c819c681c30e7c5eb56969b4731
ElinaMalice <1721537@mukilteo.wednet.edu>
2025-04-14 23:47:17 -0700

    Enemy AI

--- Parabasis/Assets/Scripts/Enemy Scripts/FlyingEnemyPatrol.cs
+++ Parabasis/Assets/Scripts/Enemy Scripts/FlyingEnemyPatrol.cs
@@ -7,21 +7,26 @@ public class FlyingEnemyPatrol : MonoBehaviour
     public GameObject pointB;
     public GameObject pointC;
     public GameObject pointD;
+    public Transform target;
+    public RaycastHit2D hit;
     public Vector2 currentDestination;
+    public Vector2 direction;
     public Vector2 check;
     public Vector2 path;
-    [SerializeField] private float detectionRange;
     [SerializeField] private float correctionVelocity;
     [SerializeField] private float velocity;
     [SerializeField] private float radius;
+    public bool trigger;
     private Rigidbody2D _rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
+        trigger = false;
         currentDestination = pointB.transform.position;
+        target = pointB.transform;
+        check = target.position - transform.position;
     }
 
     // Update is called once per frame
@@ -30,28 +35,46 @@ public class FlyingEnemyPatrol : MonoBehaviour
         if (transform.position.x == pointA.transform.position.x)
         {
             currentDestination = pointB.transform.position;
-            check = pointB.transform.position - transform.position;
+            target = pointB.transform;
+            check = target.position - transform.position;
         }
         if (transform.position.x == pointB.transform.position.x)
         {
             currentDestination = pointC.transform.position;
-            check = pointC.transform.position - transform.position;
+            target = pointC.transform;
+            check = target.position - transform.position;
         }
         if (transform.position.x == pointC.transform.position.x)
         {
             currentDestination = pointD.transform.position;
-            check = pointD.transform.position - transform.position;
+            target = pointD.transform;
+            check = target.position - transform.position;
         }
         if (transform.position.x == pointD.transform.position.x)
         {
             currentDestination = pointA.transform.position;
-            check = pointA.transform.position - transform.position;
+            target = pointA.transform;
+            check = target.position - transform.position;
         }
 
-        Vector2 direction = check.normalized;
+        direction = check.normalized;
 
-        if (Physics2D.CircleCast(transform.position, radius, direction, detectionRange))
+        if (trigger)
         {
+            _rb.linearVelocity = path * correctionVelocity;
+            StopCoroutine(Coroutine());
+        }
+
+        if (Physics2D.CircleCast(transform.position, radius, direction, 0))
+        {
+            //check = target.position - transform.position;
+            //direction = check.normalized;
+
+            hit = Physics2D.CircleCast(transform.position, radius, direction, 0);
+            check = hit.transform.position - transform.position;
+            direction = check.normalized;
+            Debug.DrawRay(transform.position, direction);
+
             float seeX = direction.x;
             float seeY = direction.y;
 
@@ -74,11 +97,66 @@ public class FlyingEnemyPatrol : MonoBehaviour
             }
             */
 
-            _rb.linearVelocity = path * correctionVelocity;
+
+            if (seeX > 0 && seeY > 0)
+            {
+                if (seeY > seeX)
+                {
+                    path = new Vector2(1, -0.5f);
+                }
+                else
+                {
+                    path = new Vector2(-0.5f, 1);
+                }
+            }
+            if (seeX > 0 && seeY < 0)//
+            {
+                if (seeX > -seeY)
+                {
+                    path = new Vector2(-0.5f, 1);
+                }
+                else
+                {
+                    path = new Vector2(1, -0.5f);
+                }
+            }
+            if (seeX < 0 && seeY < 0)
+            {
+                if (seeX > seeY)
+                {
+                    path = new Vector2(1, -0.5f);
+                }
+                else
+                {
+                    path = new Vector2(-0.5f, 1);
+                }
+            }
+            if (seeX < 0 && seeY > 0)//
+            {
+                if (-seeX > seeY)
+                {
+                    path = new Vector2(-0.5f, 1);
+                }
+                else
+                {
+                    path = new Vector2(1, -0.5f);
+                }
+            }
+
+            StartCoroutine(Coroutine());
         }
         else
         {
             transform.position = Vector2.MoveTowards(transform.position, currentDestination, velocity);
         }
+    }
+
+    IEnumerator Coroutine()
+    {
+        trigger = true;
+
+        yield return new WaitForSeconds(1);
+
+        trigger = false;
     }
 }
