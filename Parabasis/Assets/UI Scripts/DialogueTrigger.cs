537ae3a05ffa52a3d86db74ae9f94ae8ff949e4c
MUKILTEO\1714309 <1714309@mukilteo.wednet.edu>
2025-04-30 10:24:01 -0700

    hi

--- Parabasis/Assets/UI Scripts/DialogueTrigger.cs
+++ Parabasis/Assets/UI Scripts/DialogueTrigger.cs
@@ -1,16 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
     {
-        
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
