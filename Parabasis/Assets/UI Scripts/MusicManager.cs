c7279c17498604beb4ba9a0f8a4a7c0ffda24897
MUKILTEO\1714309 <1714309@mukilteo.wednet.edu>
2025-05-06 09:33:02 -0700

    hi

---
+++ Parabasis/Assets/UI Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------- Audio Source ----------")]
+    public AudioClip background;
+
+
+    public static MusicManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
